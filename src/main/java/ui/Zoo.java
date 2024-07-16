cc7e3beead08605c323c9f250013e7b88040c95b
Bohdan Danylov <bohdandanylov@gmail.com>
2024-07-15 19:50:27 -0700

    v1.3

--- src/main/java/ui/Zoo.java
+++ src/main/java/ui/Zoo.java
@@ -1,5 +1,6 @@
 package ui;
 
+import exceptions.BrokeException;
 import model.Animal;
 import model.Keeper;
 import model.Manager;
@@ -20,7 +21,11 @@ public class Zoo {
         Keeper keeper = new Keeper(animals);
         Manager manager = new Manager(animals, keeper);
 
-        manager.manage();
+        try {
+            manager.manage();
+        } catch (BrokeException e) {
+            System.out.println("Reduce costs!");;
+        }
 
     }
 }
